8fcd19e35497899097dacc549a5432a439f14106
ZUNMASUI <katyou6340@gmail.com>
2021-02-03 17:16:56 +0900

    first commit

---
+++ pages/action.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">{‌{ name }}</div>
+</template>
+ 
+<script lang="ts">
+import Vue from 'vue'
+ 
+export default Vue.extend({
+  data: () => ({
+    name: 'こんにちは',
+  }),
+})
+</script>
+ 
+<style></style>
